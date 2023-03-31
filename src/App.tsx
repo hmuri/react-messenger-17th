02874185ddcc2d0c6d6d4fce8ee8c176e8fa50e7
hmuri <113418916+hmuri@users.noreply.github.com>
2023-03-31 11:20:59 +0900

    [feat] InputBox 생성 및 Recoil 도입

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
+import {useState} from 'react';
+import styled from 'styled-components';
+import InputBox from './components/InputBox';
+import {useRecoilState} from 'recoil';
+import { chatState, IChatTypes } from './Recoil/chat';
+import ChatList from './components/ChatList';
+
+
+const Container = styled.div`
+    background-color: grey;
+    display: flex;
+    width: 25rem;
+    margin: auto;
+    height: 40rem;
+
+    flex-direction : column;
+    border : solid 1px black;
+`
+
+const ChatBox = styled.div`
+    background-color: #9bbbd4;
+    width : 100%;
+    height: 42rem;
+`
+// 확장할 떄 App.tsx => ./pages/ChatRoom.tsx로 옮기기
+
+
 function App() {
+  const [chats, setChats] = useRecoilState<IChatTypes[]>(chatState);
+  /*const addChatList = (id: number, text : string) => {
+    const chat ={
+      id : getId,
+      text : text
+    }
+    setChats(chats => chats.concat(chat));
+    getId++;
+  };*/
   return (
-    <div>
-      React Messanger
-    </div>
+    <Container>
+      <ChatBox>
+        <ChatList/>
+      </ChatBox>
+      <InputBox/>
+    </Container>
   );
 }
 
